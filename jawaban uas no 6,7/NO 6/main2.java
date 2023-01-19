54906e40747e462a1fc6f8f122217d2b483d894f
sanifaaaa <115127468+sanifaaaa@users.noreply.github.com>
2023-01-19 11:40:56 +0700

    Add files via upload

---
+++ jawaban uas no 6,7/NO 6/main2.java
@@ -0,0 +1,16 @@
+public class main2
+{
+    public static void main(String[] args){
+        for (int i = 0; i <= 4; i++){
+            for (int j = 0; j<= 4-i; j++){
+                System.out.print( " ");
+            }
+            for (int k = 0; k <= i; k++){
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+    }
+}
+    
+
---
+++ jawaban uas no 6,7/NO 6/main3.java
@@ -0,0 +1,14 @@
+public class main3{
+    public static void main(String[] args){
+        for(int i = 1; i<=5; i++ ){
+    for (int j = 1; j<= 5-1; j++){
+        System.out.print(" ");
+    }
+    for(int k = i; k >= 1; k--){
+        System.out.print(k);
+    }
+    System.out.println();
+
+        }
+    }
+}
